193c5c1b9834ef15d813a9a82ba4f363a2035589
guillermounivalle <guillermo.hernadez@correounivalle.edu.co>
2023-11-13 00:55:14 -0500

    Connections has been created

---
+++ src/app.js
@@ -0,0 +1,48 @@
+/**
+ * Imprt libraries and package
+ */
+
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+//const cors = require('cors');
+const {DB_HOST, DB_NAME, PORT, API_NAME, API_VERSION} = require('./config');
+
+//Express
+const app = express();
+
+
+//Config module
+const config = require('./config');
+
+//SET PORT LISTENING
+app.set('port', process.env.PORT || PORT)
+
+//init app connections
+async function start (){
+    try{
+        //add port
+        app.listen(app.get('port'), () => console.log(`Server running on port ${app.get('port')}`));
+
+    }catch(err){
+        console.log('Error ===> ', err);
+        process.exit(1);
+    };
+};
+
+
+//URL BASE
+const baseUrl = `/${API_NAME}/${API_VERSION}`
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+//.use(cors());
+
+//Routes
+const indexRouter = require('./routes/index');
+
+//instances routers
+app.use(baseUrl, indexRouter);
+
+start();
+module.exports = app;
---
+++ src/config.js
@@ -0,0 +1,5 @@
+// info configuration server
+
+require('dotenv').config();
+
+module.exports = {DB_NAME, PORT, API_NAME, API_VERSION} = process.env;
